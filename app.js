c98aed41848515c7f6b7465049df33e27b04cda6
leowrites <75044178+leowrites@users.noreply.github.com>
2022-08-20 16:47:17 -0400

    Added server files

---
+++ app.js
@@ -0,0 +1,51 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const { createServer } = require("http")
+const { Server } = require("socket.io")
+
+const { answerQuestion } = require('./controllers/questionHandler')
+
+const app = express();
+const httpServer = createServer(app)
+const io = new Server(httpServer)
+
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use(express.static(path.join(__dirname, '/client/build')));
+
+// app.use((req, res) => {
+//   res.sendFile(path.join(__dirname, '/client/build/index.html'));
+// });
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+io.on("connection", (socket) => {
+  socket.on('question', answerQuestion)
+})
+
+httpServer.listen(8000)
+
+module.exports = app;
---
+++ controllers/questionHandler.js
@@ -0,0 +1,6 @@
+// handle the questions routes
+module.exports = (io) => {
+    const answerQuestion = async function(question) {
+
+    }
+}
